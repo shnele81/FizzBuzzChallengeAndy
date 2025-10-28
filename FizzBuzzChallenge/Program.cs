32b27c60f4b9b40f5b0418998e747a8f5b0a80a5
Andrew ONeill <andy@evolve757.com>
2025-10-28 09:51:08 -0400

    Initial commit

---
+++ FizzBuzzChallenge/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string FizzBuzz(int number)
+{
+    var returnAnswer = "";
+    if (number % 3 == 0)
+    {
+        returnAnswer = "Fizz";
+    }
+
+    if (number % 5 == 0)
+    {
+        returnAnswer = "Buzz";
+    }
+
+    if (number % 3 == 0 && number % 5 == 0)
+    {
+        returnAnswer = "FizzBuzz";
+    }
+
+    return returnAnswer;
+}
